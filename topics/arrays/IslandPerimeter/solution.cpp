32eecc5d278126a39cdfc7c8ae2fe4b9ffc18713
aditya7876 <iamdaredevil96@gmail.com>
2026-09-26 23:28:51 +0530

    day 3: solve array problems

---
+++ topics/arrays/IslandPerimeter/solution.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        int perimeter = 0;
+
+        for (int i = 0 ; i < rows; i++) {
+            for (int j = 0 ; j < cols; j++) {
+                if (grid[i][j] == 1) {
+                    perimeter += 4;
+                    if (i > 0 && grid[i - 1][j] == 1) perimeter--;
+                    if (i < rows - 1 && grid[i+1][j] == 1) perimeter--;
+                    if (j > 0 && grid[i][j-1] == 1) perimeter--;
+                    if (j < cols - 1 && grid[i][j+1]) perimeter--;
+                }
+            }
+        }
+        return perimeter;
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<vector<int>> grid = {
+        {0, 1, 0, 0},
+        {1, 1, 1, 0},
+        {0, 1, 0, 0},
+        {1, 1, 0, 0}
+    };
+
+    int result = solution.islandPerimeter(grid);
+    cout << "Island Perimeter: " << result << endl;
+
+    return 0;
+}
